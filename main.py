b8268da545ac61f88dde0207be51343c11e8614a
djeck1432 <oleh.kostashchuk@gmail.com>
2020-07-03 14:08:11 +0200

    canceled approved ssl-certificate

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import asyncio
 
 
 async def fetch(session, url):
-    async with session.get(url) as response:
+    async with session.get(url,ssl=False) as response:
         response.raise_for_status()
         return await response.text()
 
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        html = await fetch(session, 'http://example.com')
+        html = await fetch(session, 'https://inosmi.ru/politic/20200702/247694385.html')
         print(html)
 
 
