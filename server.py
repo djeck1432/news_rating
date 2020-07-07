cf2fe98b7c0b5476f71c5733f1c431f7eb829880
djeck1432 <oleh.kostashchuk@gmail.com>
2020-07-07 22:38:03 +0200

    connected two parts of code: client and server

--- server.py
+++ server.py
@@ -1,13 +1,23 @@
+import main
 from aiohttp import web
 
-async def handle(request):
-    name = request.match_info.get('name','Anonymous')
-    text = 'Hello, ' + name
-    return web.Response(text=text)
+
+async def handle(request): #FIXME
+    data = request.query
+    dirty_urls = data['urls'].split(',')
+    clean_urls = [url for url in dirty_urls if url != '']
+    if len(clean_urls) >= 0:
+        error_response = {"error": "too many urls in request, should be 10 or less"}
+        #TODO добавить формат ответа json
+        raise web.HTTPBadRequest(text='error', content_type='application/json')
+    else:
+        await main.main(*clean_urls)
+        response = main.articles_data
+        return web.json_response(response)
 
 app = web.Application()
-app.add_routes([web.get('/',handle),
-                web.get('/{name}', handle)])
+app.add_routes([web.get('/', handle),
+                web.get('/400',handle)])
 
 
 if __name__=='__main__':
