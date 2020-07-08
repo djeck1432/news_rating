5fc7991fbb40729e49681d9ce353ac45694b67cb
djeck1432 <oleh.kostashchuk@gmail.com>
2020-07-08 06:45:29 +0200

    del "else" in handle func

--- server.py
+++ server.py
@@ -6,14 +6,14 @@ async def handle(request): #FIXME
     data = request.query
     dirty_urls = data['urls'].split(',')
     clean_urls = [url for url in dirty_urls if url != '']
-    if len(clean_urls) >= 0:
+    if len(clean_urls) >= 2:
         error_response = {"error": "too many urls in request, should be 10 or less"}
         #TODO добавить формат ответа json
         raise web.HTTPBadRequest(text='error', content_type='application/json')
-    else:
-        await main.main(*clean_urls)
-        response = main.articles_data
-        return web.json_response(response)
+
+    await main.main(*clean_urls)
+    response = main.articles_data
+    return web.json_response(response)
 
 app = web.Application()
 app.add_routes([web.get('/', handle),
