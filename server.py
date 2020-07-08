5ab09a8552900a9d173ac52b182fe92675c32a56
djeck1432 <oleh.kostashchuk@gmail.com>
2020-07-08 06:50:56 +0200

    send error_response like json

--- server.py
+++ server.py
@@ -6,10 +6,9 @@ async def handle(request): #FIXME
     data = request.query
     dirty_urls = data['urls'].split(',')
     clean_urls = [url for url in dirty_urls if url != '']
-    if len(clean_urls) >= 2:
+    if len(clean_urls) >= 0:
         error_response = {"error": "too many urls in request, should be 10 or less"}
-        #TODO добавить формат ответа json
-        raise web.HTTPBadRequest(text='error', content_type='application/json')
+        return web.json_response(error_response,status=400)
 
     await main.main(*clean_urls)
     response = main.articles_data
