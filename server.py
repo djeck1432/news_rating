88c471082e00d11bb0d69bb562751634d2a1539a
djeck1432 <oleh.kostashchuk@gmail.com>
2020-07-08 06:52:33 +0200

    del unnecessary comments and indentations

--- server.py
+++ server.py
@@ -2,11 +2,11 @@ import main
 from aiohttp import web
 
 
-async def handle(request): #FIXME
+async def handle(request):
     data = request.query
     dirty_urls = data['urls'].split(',')
     clean_urls = [url for url in dirty_urls if url != '']
-    if len(clean_urls) >= 0:
+    if len(clean_urls) >= 10:
         error_response = {"error": "too many urls in request, should be 10 or less"}
         return web.json_response(error_response,status=400)
 
@@ -15,8 +15,7 @@ async def handle(request): #FIXME
     return web.json_response(response)
 
 app = web.Application()
-app.add_routes([web.get('/', handle),
-                web.get('/400',handle)])
+app.add_routes([web.get('/', handle)])
 
 
 if __name__=='__main__':
