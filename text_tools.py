0b6200d6f5b7e85ca9a22e0e2101cb449ab96f7a
djeck1432 <oleh.kostashchuk@gmail.com>
2020-07-09 20:53:09 +0200

    split_by_words made asyncio and rewrote test for this func

--- text_tools.py
+++ text_tools.py
@@ -1,5 +1,7 @@
 import pymorphy2
 import string
+from async_timeout import timeout
+import asyncio
 
 
 def _clean_word(word):
@@ -9,7 +11,7 @@ def _clean_word(word):
     return word
 
 
-def split_by_words(morph, text):
+async def split_by_words(morph, text):
     """Учитывает знаки пунктуации, регистр и словоформы, выкидывает предлоги."""
     words = []
     for word in text.split():
@@ -17,6 +19,7 @@ def split_by_words(morph, text):
         normalized_word = morph.parse(cleaned_word)[0].normal_form
         if len(normalized_word) > 2 or normalized_word == 'не':
             words.append(normalized_word)
+        await asyncio.sleep(0)
     return words
 
 
@@ -25,9 +28,9 @@ def test_split_by_words():
     # Старайтесь организовать свой код так, чтоб создавать экземпляр MorphAnalyzer заранее и в единственном числе
     morph = pymorphy2.MorphAnalyzer()
 
-    assert split_by_words(morph, 'Во-первых, он хочет, чтобы') == ['во-первых', 'хотеть', 'чтобы']
+    assert  asyncio.run(split_by_words(morph, 'Во-первых, он хочет, чтобы')) == ['во-первых', 'хотеть', 'чтобы']
 
-    assert split_by_words(morph, '«Удивительно, но это стало началом!»') == ['удивительно', 'это', 'стать', 'начало']
+    assert  asyncio.run(split_by_words(morph, '«Удивительно, но это стало началом!»')) == ['удивительно', 'это', 'стать', 'начало']
 
 
 def calculate_jaundice_rate(article_words, charged_words):
@@ -46,3 +49,6 @@ def calculate_jaundice_rate(article_words, charged_words):
 def test_calculate_jaundice_rate():
     assert -0.01 < calculate_jaundice_rate([], []) < 0.01
     assert 33.0 < calculate_jaundice_rate(['все', 'аутсайдер', 'побег'], ['аутсайдер', 'банкротство']) < 34.0
+
+# if __name__=='__main__':
+#     asyncio.run(split_by_words)
